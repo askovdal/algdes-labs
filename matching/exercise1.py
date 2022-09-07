d5caa307f40dfddd48201a4c5c121e3dcff8d801
Mårtensson, Laura Vikke <9077020219@hanyang.ac.kr>
2022-09-07 15:23:33 +0200

    new data structures

--- matching/exercise1.py
+++ matching/exercise1.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 ## INPUT
 
@@ -12,7 +13,7 @@ names = {}
 proposers = {}
 rejecters = {}
 matchings = {}
-free = []
+free = set()
 
 for i in range(n*2):
     person = [j for j in input[i].split()]
@@ -23,30 +24,37 @@ input = input[n*2:]
 for i in range(n*2):
     if (int(input[i].split(':')[0])%2) == 0:
         woman = [j for j in input[i].split(':')]
-        rejecters[int(woman[0])] = [int(i) for i in woman[1].split()]
+        rejecters[int(woman[0])] = {int(man): rank for rank, man in enumerate(woman[1].split())}
+        matchings[int(woman[0])] = None
     else:
         man = [j for j in input[i].split(':')]
-        free.append(int(man[0]))
-        proposers[int(man[0])] = [int(i) for i in man[1].split()]
-    
+        free.add(int(man[0]))
+        proposers[int(man[0])] = deque([int(i) for i in man[1].split()])
+print('proposers, men:', proposers, '\n')
+print('rejesters, women:', rejecters, '\n')
 
 
 ### ALGORITHM
 
 while len(free) != 0: #While there is a man m who is free and hasn't proposed to every woman
-    man = free[0] #Choose such a man m
-    woman = proposers[man][0] #Let w be the highest-ranked woman in m's preference list
+    man = free.pop() #Choose such a man m
+    woman = proposers[man][0]#Let w be the highest-ranked woman in m's preference list
+    print('woman', woman, 'man', man)
+    print('proposers, men:', proposers, '\n')
+    print('rejesters, women:', rejecters, '\n')
 
-    if woman not in matchings: #If w is free then
+    if matchings[woman] == None: #If w is free then
         matchings[woman] = man #(m, w) become engaged
-        del free[0] #remove man from list of free men
-        proposers[man].remove(woman) #remove woman from list of women man has proposed to
-    elif rejecters[woman].index(man) > rejecters[woman].index(matchings[woman]): # Else w is currently engaged to m and If w prefers old m to new m then
-        proposers[man].remove(woman) #m remains free and remove the woman that he has proposed to from rank list
+        proposers[man].popleft() #remove woman from list of women man has proposed to
+        
+    elif rejecters[woman][man] > rejecters[woman][matchings[woman]]: # Else w is currently engaged to m and If w prefers old m to new m then
+       
+        proposers[man].popleft() #m remains free and remove the woman that he has proposed to from rank list
+        free.add(man)
     else: #w prefers new m to old m
-        free.append(matchings[woman]) #add currently matched man to list of free men
+        free.add(matchings[woman]) #add currently matched man to list of free men
         matchings[woman] = man #add new man to matching
-        del free[0] #remove new man from free men list
+     
         
 for woman, man in matchings.items():
     print(names[man] + ' -- ' + names[woman])      
