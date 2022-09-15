d950156bca6e8fa63f01f74c809da9fe55b1f442
Mårtensson, Laura Vikke <9077020219@hanyang.ac.kr>
2022-09-15 20:41:01 +0200

    updates

--- closest-points/src/main.py
+++ closest-points/src/main.py
@@ -11,60 +11,94 @@ expression = (
 
 matches = re.findall(expression, raw)
 
-points_x = [list(map(float, p)) for p in matches]
-points_y = copy.deepcopy(points_x)
+def euclidian_distance(p1, p2):
+    return math.sqrt((p1[1] - p2[1]) * (p1[1] - p2[1]) + (p1[2] - p2[2]) * (p1[2] - p2[2]))
 
-# Sorted by x-coordinates
-points_x.sort(key=lambda x: x[0])
-
-# Sorted by y-coordinates
-points_y.sort(key=lambda x: x[1])
-
-print(points_x)
-print(points_y)
-
-# Divide sorted x coordinates down the middle to find line L
-split = len(points_x) // 2
-Q = points_x[:split]  # Left side of line L
-R = points_x[split:]  # Right side of Line L
-
-# Q sorted by x
-Q_x = Q
-# Q sorted by y
-Q_y = sorted(Q, key=lambda x: x[1])
-
-print(Q_x)
-print(Q_y)
-
-# R sorted by x
-R_x = R
-# R sorted by y
-R_y = sorted(R, key=lambda x: x[1])
-
-print(R_x)
-print(R_y)
-
-
-def dist(p1, p2):
-    return math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]))
-
-
-# A Brute Force method to return the
-# smallest distance between two points
-# in P[] of size n
-def find_smallest_distance(Q, n):
-    min_val = float('inf')
-    for i in range(n):
+def smallest_dist(P, n): 
+    min_val = float('inf') # base value 
+    p_0 = 0 
+    p_1 = 0
+    for i in range(n): # Nested loop to compare all points in the list (max 3 points = max )
         for j in range(i + 1, n):
-            if dist(Q[i], Q[j]) < min_val:
-                min_val = dist(Q[i], Q[j])
+            if euclidian_distance(P[i], P[j]) < min_val:
+                min_val = euclidian_distance(P[i], P[j])
+                p_0 = P[i]
+                p_1 = P[j]
     return min_val
 
+def smallest_dist_middle(S_y, n):
+    min_val = float('inf')
+    p_0 = 0 
+    p_1 = 0
+    count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if count == 15:
+                count = 0
+                break
+            elif euclidian_distance(S_y[i], S_y[j]) < min_val:
+                min_val = euclidian_distance(S_y[i], S_y[j])
+                p_0 = S_y[i]
+                p_1 = S_y[j]
+            count += 1
+    return min_val
 
-def closest_pair_rec(P_x, P_y):
-    if len(P_x) <= 3:
-        find_smallest_distance()
+def closest_pair(P):
+    p_x = [list(map(float, p)) for p in P]
+    p_y = copy.deepcopy(p_x)
+    # Sorted by x-coordinates
+    p_x.sort(key=lambda x: x[1])
+    # Sorted by y-coordinates
+    p_y.sort(key=lambda x: x[2])
+    
+    return closest_pair_rec(p_x, p_y, 'O')
+    
+def closest_pair_rec(p_x, p_y, m):
+    
+    if len(p_x) <= 3:
+        
+        
+        return smallest_dist(p_x, len(p_x))
+        
+    # Else, divide and construct Qx, Qy, Rx, Ry (O(n) time)
+    # Divide sorted x coordinates down the middle to find line L
+    split = len(p_x) // 2
+    # Left side of line L
+    Q_x = p_x[:split]  
+    Q_y = p_y[:split]
+    # Right side of Line L
+    R_x = p_x[split:]  
+    R_y = p_y[split:] 
+    
+    q_min = closest_pair_rec(Q_x, Q_y, 'Q') # Return the two points in Q with smallest distance
+   
+    r_min = closest_pair_rec(R_x, R_y, 'R') # Return the two points in R with smallest distance
+    
+    
+    delta = min(q_min, r_min) # smallest distance of the two halves Q & L
+    
+    x = p_x[split] # the point with the x-coordinate that is in the middle of the plane and defines L
+    
+    
+    
+    
+    S_y = [point for point in p_y if (abs(point[1] - x[1]) < delta)] # set of all points in P, sorted by y coordinate, within delta distance of L
+    s_min = smallest_dist_middle(S_y, len(S_y))
+    
+    
+    
+    if s_min < delta:   
+        return s_min
+    elif q_min < r_min: 
+        return q_min
+    else:
+        return r_min
+    
+    
+    
 
+min_distance = closest_pair(matches)
+print(len(matches), min_distance)
 
 
 """
