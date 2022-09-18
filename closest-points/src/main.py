0c1ead0da0174c2355b30e9386ea531cb6430dc3
Andreas Skovdal <andreasskovdal@gmail.com>
2022-09-18 12:29:59 +0200

    Fix the way Qx, Qy, Rx, Ry is constructed, and use dicts

--- closest-points/src/main.py
+++ closest-points/src/main.py
@@ -1,20 +1,15 @@
-import copy
-import re
-import sys
 import math
-
-raw = "".join(list(sys.stdin))
+import os
+import re
 
 expression = (
     "(\d+)\s+([-+]?\d*\.?\d+(?:[eE][-+]?\d+)?)\s+([-+]?\d*\.?\d+(?:[eE][-+]?\d+)?)"
 )
 
-matches = re.findall(expression, raw)
-
 
 def euclidian_distance(p1, p2):
     return math.sqrt(
-        (p1[1] - p2[1]) * (p1[1] - p2[1]) + (p1[2] - p2[2]) * (p1[2] - p2[2])
+        (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1])
     )
 
 
@@ -23,42 +18,69 @@ def smallest_dist(P, n):
     # Nested loop to compare all points in the list (max 3 points = max )
     for i in range(n):
         for j in range(i + 1, n):
-            if euclidian_distance(P[i], P[j]) < min_val:
-                min_val = euclidian_distance(P[i], P[j])
+            dist = euclidian_distance(P[i], P[j])
+            if dist < min_val:
+                min_val = dist
+
     return min_val
 
 
 def smallest_dist_middle(S_y, n):
     min_val = float("inf")
     count = 0
-    # Nested loop that only compare with the next 15 points (look in the book)
+    # Nested loop that only compare with the next 15 points (see book p. 135 5.10)
     for i in range(n):
         for j in range(i + 1, n):
             if count == 15:
                 count = 0
                 break
-            elif euclidian_distance(S_y[i], S_y[j]) < min_val:
-                min_val = euclidian_distance(S_y[i], S_y[j])
+
+            dist = euclidian_distance(S_y[i], S_y[j])
+            if dist < min_val:
+                min_val = dist
+
             count += 1
+
     return min_val
 
 
-def closest_pair_rec(p_x, p_y):
-
-    if len(p_x) <= 3:
+def closest_pair_rec(P_x, P_y):
+    if len(P_x) <= 3:
         # return the smallest distance between the three points and continue the recursion
-        return smallest_dist(p_x, len(p_x))
+        return smallest_dist(list(P_x.values()), len(P_x))
 
     # Else, divide and construct Qx, Qy, Rx, Ry (O(n) time):
 
     # Divide sorted x coordinates down the middle to find line L
-    split = len(p_x) // 2
+    split = len(P_x) // 2
+
     # Left side of line L
-    Q_x = p_x[:split]
-    Q_y = p_y[:split]
+    Q = dict(list(P_x.items())[:split])
+
     # Right side of Line L
-    R_x = p_x[split:]
-    R_y = p_y[split:]
+    R = dict(list(P_x.items())[split:])
+
+    # Q_x is the same as Q, sorted by increasing x-coordinate
+    # R_x is the same as R, sorted by increasing x-coordinate
+    # Q and R are already sorted by increasing x-coordinate, so we don't need to
+    # do anything to create Q_x and R_x
+    Q_x = Q
+    R_x = R
+
+    # Q_y is the same as Q, sorted by increasing y-coordinate
+    # R_y is the same as R, sorted by increasing y-coordinate
+    # To create Q_y and R_y, we do a single pass through P_y, as those points
+    # are already sorted by increasing y-coordinate
+    Q_y = {}
+    R_y = {}
+    for point in P_y:
+        try:
+            # If the point is in Q (the left side of line L), we add it to Q_y
+            Q_y[point] = Q[point]
+        except KeyError:
+            # If the point is not in Q, we know that it's in R (the right side
+            # of line L), so we add it to R_y
+            R_y[point] = R[point]
 
     q_min = closest_pair_rec(Q_x, Q_y)  # Return the min distance in Q
     r_min = closest_pair_rec(R_x, R_y)  # Return the min distance in R
@@ -66,10 +88,11 @@ def closest_pair_rec(p_x, p_y):
     delta = min(q_min, r_min)  # smallest distance of the two halves Q & L
 
     # the point with the x-coordinate that is in the middle of the plane and defines L
-    x = p_x[split]
+    x = list(P_x.values())[split]
 
     # set of all points in P, sorted by y coordinate, within delta distance of L
-    S_y = [point for point in p_y if (abs(point[1] - x[1]) < delta)]
+    S_y = [point for point in list(P_y.values()) if (abs(point[0] - x[0]) < delta)]
+
     # Return the min distance in the middle section
     s_min = smallest_dist_middle(S_y, len(S_y))
 
@@ -83,18 +106,35 @@ def closest_pair_rec(p_x, p_y):
 
 
 def closest_pair(P):
-    p_x = [list(map(float, p)) for p in P]
-    p_y = copy.deepcopy(p_x)
-    # Sorted by x-coordinates
-    p_x.sort(key=lambda x: x[1])
-    # Sorted by y-coordinates
-    p_y.sort(key=lambda x: x[2])
+    # Sorted by x-coordinates (O(n log n) time)
+    P_x = sorted(P, key=lambda x: x[1])
 
-    return closest_pair_rec(p_x, p_y)
+    # Sorted by y-coordinates  (O(n log n) time)
+    P_y = sorted(P, key=lambda x: x[2])
+
+    # Transform the lists into dicts so we can index the points in constant time
+    P_x = {int(p[0]): p[1:] for p in P_x}
+    P_y = {int(p[0]): p[1:] for p in P_y}
+
+    return closest_pair_rec(P_x, P_y)
 
 
-min_distance = closest_pair(matches)
-print(len(matches), min_distance)
+if __name__ == "__main__":
+    data_files = os.listdir("../data")
+    # Remove files that don't end in "-tsp.txt"
+    tsp_files = list(filter(lambda x: x.endswith("-tsp.txt"), data_files))
+    tsp_files.sort()
+
+    for tsp_file in tsp_files:
+        filename = f"../data/{tsp_file}"
+        with open(filename, "r") as file:
+            data = file.read()
+            # Find points using the regex from the hint
+            matches = re.findall(expression, data)
+            matches = [list(map(float, match)) for match in matches]
+
+            min_distance = closest_pair(matches)
+            print(f"{filename}: {len(matches)} {min_distance}")
 
 
 """
